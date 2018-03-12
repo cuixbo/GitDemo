cc6e3c7ea08167fd957335ebc29735113bbd69f8
xiaobocui <cuixbo@qq.com>
2018-03-12 11:40:03 +0800

    update 5

---
+++ pgyer-upload.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COMMIT_MSG=`git log --oneline -1`
+
+URL=https://www.pgyer.com/apiv1/app/upload
+
+curl ${URL} -F _api_key=${PGYER_APIKEY} -F uKey=${PGYER_UKEY} -F file=@app/build/outputs/apk/app-pgyer-debug.apk -F updateDescription="${COMMIT_MSG}" --retry 50 --retry-delay 2 --retry-max-time 5 > /dev/null
+

c22d5fe3d8c5cc8cad41a27b862cce55304190f8
xiaobocui <cuixbo@qq.com>
2018-04-27 15:18:12 +0800

    change pgyer

--- pgyer-upload.sh
+++ pgyer-upload.sh
@@ -4,5 +4,5 @@ COMMIT_MSG=`git log --oneline -1`
 
 URL=https://www.pgyer.com/apiv1/app/upload
 
-curl ${URL} -F _api_key=${PGYER_APIKEY} -F uKey=${PGYER_UKEY} -F file=@app/build/outputs/apk/app-pgyer-debug.apk -F updateDescription="${COMMIT_MSG}" --retry 50 --retry-delay 2 --retry-max-time 5 > /dev/null
+curl ${URL} -F _api_key=${PGYER_APIKEY} -F uKey=${PGYER_UKEY} -F file=@app/build/outputs/apk/pgyer/debug/app-pgyer-debug.apk -F updateDescription="${COMMIT_MSG}" --retry 50 --retry-delay 2 --retry-max-time 5 > /dev/null
 

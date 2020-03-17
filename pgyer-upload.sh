e8ca738d290e4975413e30afde6b4a1336d5fa42
xiaobocui <cuixbo@qq.com>
2020-03-17 10:47:54 +0800

    update pgyer v2

--- pgyer-upload.sh
+++ pgyer-upload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+COMMIT_MSG=`git log --oneline -1`
 URL=https://www.pgyer.com/apiv2/app/upload
 APK_FILE=@app/build/outputs/apk/pgyer/debug/app-pgyer-debug.apk
-COMMIT_MSG=`git log --oneline -1`
 
 curl ${URL} -F "_api_key=${PGYER_APIKEY}" -F "file=${APK_FILE}" -F "buildInstallType=3" -F "buildUpdateDescription=${COMMIT_MSG}" --retry 5 --retry-delay 3 -O /dev/null
 

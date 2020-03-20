ef2739a45b5cd13e6196e114c50857afe1f75e9b
pato <pato@patodeMacBook-Pro.local>
2020-03-20 15:23:25 +0800

    change more

--- app/src/main/java/com/xbc/git/test/MainActivity.java
+++ app/src/main/java/com/xbc/git/test/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //change 20/3
+        //change 20/3 more
     }
 }
